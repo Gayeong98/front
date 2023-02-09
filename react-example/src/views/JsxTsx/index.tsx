1f20a9557dd943aa00556f8353ca19f0d90f4f13
Gayeong98 <gayeong122@gmail.com>
2023-02-09 17:27:22 +0900

    commit

--- react-example/src/views/JsxTsx/index.tsx
+++ react-example/src/views/JsxTsx/index.tsx
@@ -21,6 +21,8 @@ export default function JsxTsx() {
     
     //? return () 안에서 Typescript를 사용하려면 {}로 묶어서 작성해야 함
     //^ {}를 써서 Typescript를 작성하고 싶으면 필수적으로 html 태그 안에 있어야 함
+    
+    //? return () 안의 {} 안에서 다시 html을 표기하려면 ()로 반환해야 함
   return (
     <div>{text}</div>
   )
